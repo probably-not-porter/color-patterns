42d8c2292ad3f25ab22cb75b2f468cf72a5af454
probably-not-porter <pelibby16@earlham.edu>
2019-12-18 20:49:51 -0500

    starting work

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+
+const app = express();
+
+//app.use(favicon(path.join(__dirname, 'public/img_icons', 'favicon.png')));
+
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+    res.render("index",{
+    })
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
